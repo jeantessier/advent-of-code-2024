b60ea4e7eeda79df5e2648c3cd743e66182a4559
Jean Tessier <jean@jeantessier.com>
2024-12-01 22:05:36 -0500

    Day 01 Puzzle 2

---
+++ day01/puzzle2.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# lines = readlines
+# lines = File.readlines("sample.txt") # Answer: 31 (in 64 ms)
+lines = File.readlines("input.txt") # Answer: 20719933 (in 66 ms)
+
+original_lists = lines.map { |line| line.split(/\s+/).map(&:to_i) }
+
+first_list = original_lists.map { |lists| lists[0] }
+
+frequencies = original_lists.map { |list| list[1] }.reduce(Hash.new(0)) { |hash, item| hash[item] += 1; hash }
+
+similarity_scores = first_list.map { |value| value * frequencies[value] }
+
+similarity_score = similarity_scores.sum
+
+puts "total similarity score: #{similarity_score}"
