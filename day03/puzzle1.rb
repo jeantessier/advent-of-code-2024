6a4411f087b280309d876e6cc1069ff26eb7c8ea
Jean Tessier <jean@jeantessier.com>
2024-12-03 08:44:33 -0500

    Day 03 Puzzle 1

---
+++ day03/puzzle1.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# lines = readlines
+# lines = File.readlines("sample.txt") # Answer: 161 (in 51 ms)
+lines = File.readlines("input.txt") # Answer: 160672468 (in 59 ms)
+
+REGEX = /mul\((\d{1,3}),(\d{1,3})\)/
+
+multiplications = lines.map do |line|
+  line.scan(REGEX).map do |n1, n2|
+    n1.to_i * n2.to_i
+  end
+end
+
+total_safe = multiplications.flatten.sum
+
+puts "total safe: #{total_safe}"
