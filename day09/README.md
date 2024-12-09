b966a0fd8ca7e1b05d6af7ca1dfda68f509d575a
Jean Tessier <jean@jeantessier.com>
2024-12-09 11:02:08 -0500

    Day 09 Puzzle 1

---
+++ day09/README.md
@@ -0,0 +1,11 @@
+# [Day 09](https://adventofcode.com/2024/day/9)
+
+## Puzzle 01
+
+The input is 20,000 single digit numbers.  I would need at most 200KB to create
+a block map.  It's worth a try and runs fast enough.  I could have tracked each
+group of consecutive blocks instead, but I didn't feel the added complexity was
+worth it, yet.
+
+## Puzzle 02
+
---
+++ day09/puzzle1.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+# Login to https://adventofcode.com/2024/day/9/input to download 'input.txt'.
+
+# lines = readlines
+# lines = File.readlines('sample.txt') # Answer: 1928 (in 54 ms)
+lines = File.readlines('input.txt') # Answer: 6344673854800 (in 804 ms)
+
+disk_map = lines.first.chomp.split(//).map(&:to_i)
+
+puts 'Disk Map'
+puts '--------'
+puts disk_map
+puts
+
+blocks = []
+disk_map.each_slice(2).each_with_index do |pair, i|
+  blocks += pair[0].times.map { i }
+  blocks += pair[1].times.map { nil } if pair.size > 1
+end
+
+puts 'Blocks'
+puts '------'
+puts "| #{blocks.join(" | ")} |"
+puts
+
+first_free_index = blocks.find_index(nil)
+last_file_index = blocks.size - 1
+
+puts 'Indices'
+puts '-------'
+puts "free: #{first_free_index}, file: #{last_file_index}"
+puts
+
+while first_free_index < last_file_index
+  blocks[first_free_index], blocks[last_file_index] = blocks[last_file_index], blocks[first_free_index]
+
+  first_free_index += 1
+  first_free_index += 1 until blocks[first_free_index].nil?
+
+  last_file_index -= 1
+  last_file_index -= 1 while blocks[last_file_index].nil?
+
+  # puts "| #{blocks.join(" | ")} |"
+end
+
+checksum = 0
+blocks.compact.each_with_index do |block, i|
+  checksum += block * i
+end
+
+puts
+puts "Checksum: #{checksum}"
