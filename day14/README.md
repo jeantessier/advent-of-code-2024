12bb27740f407a0d349ce2e781734d350db63753
Jean Tessier <jean@jeantessier.com>
2024-12-13 23:14:37 -0800

    Day 14 Puzzle 2

--- day14/README.md
+++ day14/README.md
@@ -10,4 +10,25 @@ I wanted to experiment with an immutable structure, for a change.
 
 ## Puzzle 02
 
+I printed the map after each move, driving it forward by pressing `enter`
+repeatedly.  I looked for a pattern to slowly coalesce, but it was taking a long
+time.
 
+I thought there might be something with the periodicity of each robot's
+movement.  But without a clue as to what to look for, there wasn't much I could
+do there.
+
+I tried to do binary search using the Advent of Code website.  Whenever I
+submitted a guess, it would tell me "too low" or "too high".  With this, I found
+out it was **between 5,000 and 10,000**.  When I tried 7,500, it told me I
+essentially had "too many guesses", and stopped being helpful.
+
+I looked at the
+[subreddit](https://www.reddit.com/r/adventofcode/comments/1hdw23y/2024_day_14_part_2_what/)
+for inspiration.  One recurring heuristic was to look for a bunch (10+) of
+robots forming a horizontal line.
+
+So, I looped through `5_000..10_000` seconds and for each computed the position
+of each robot, looked for a row of 13+ robots with 13+ being side-by-side.  This
+yielded **4 candidates**.  I printed them out and picked the one that showed a
+Christmas tree.
---
+++ day14/puzzle2.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+
+# Login to https://adventofcode.com/2024/day/14/input to download 'input.txt'.
+
+CONSTANTS = {
+  file: 'input.txt', x_size: 101, y_size: 103, answer: 7753, time: '1,172 ms',
+}
+
+lines = File.readlines(CONSTANTS[:file])
+
+Robot = Data.define(:x, :y, :velocity_x, :velocity_y) do
+  def coord
+    [x, y]
+  end
+
+  def move(n = 1)
+    Robot.new(
+      (x + (n * velocity_x)) % CONSTANTS[:x_size],
+      (y + (n * velocity_y)) % CONSTANTS[:y_size],
+      velocity_x,
+      velocity_y,
+    )
+  end
+
+  def to_s
+    "(#{x}, #{y})"
+  end
+end
+
+ROBOT_REGEX = /p=(?<start_x>\d+),(?<start_y>\d+) v=(?<velocity_x>-?\d+),(?<velocity_y>-?\d+)/
+
+robots = lines.map { |line| ROBOT_REGEX.match(line) }.compact.map do |match|
+  Robot.new match[:start_x].to_i, match[:start_y].to_i, match[:velocity_x].to_i, match[:velocity_y].to_i
+end
+
+def print_map(robots)
+  grid = robots.map(&:coord).reduce(Hash.new { |h, k| h[k] = [] }) do |map, coord|
+    map[coord] << coord
+    map
+  end
+
+  (0...CONSTANTS[:y_size]).each do |y|
+    puts (0...CONSTANTS[:x_size]).map { |x| grid[[x, y]].empty? ? '.' : grid[[x, y]].size }.join('')
+  end
+end
+
+puts 'Robots'
+puts '------'
+print_map(robots)
+puts robots
+puts
+
+# Looks for a row of 13+ robots with 13+ of them side-by-side.
+def analyze(robots)
+  robots
+    .group_by { |robot| robot.y }
+    .select { |_, v| v.size >= 13 }
+    .any? do |_, robots|
+      robots
+        .map(&:x)
+        .sort
+        .each_cons(2)
+        .collect { |x1, x2| (x2 - x1).abs }
+        .count(1) >= 13
+    end
+end
+
+candidates = (5_000..10_000).select do |seconds|
+  analyze(robots.map { |robot| robot.move(seconds) })
+end
+
+puts 'Candidates'
+puts '----------'
+candidates.each do |candidate|
+  puts "candidate #{candidate}"
+  print_map(robots.map { |robot| robot.move(candidate) })
+  puts
+end
+puts
+
+puts "Number of Candidates: #{candidates.size}"
