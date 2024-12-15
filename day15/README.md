687f4922c6a636a39b945119b3033452de0fcbf6
Jean Tessier <jean@jeantessier.com>
2024-12-15 00:02:09 -0800

    Day 15 Puzzle 2

--- day15/README.md
+++ day15/README.md
@@ -8,8 +8,92 @@ an obstacle or free space, that was easy to handle.  If it moved into a box,
 then it recursively attempted to move the box first and the robot only if the
 space became empty.
 
-Printing the map after each move made the runtime **over 2s**.  Removing the
+Printing the map after each move made the runtime **over 2,000ms**.  Removing the
 trace statements took the runtime down to **135ms**.
 
 ## Puzzle 02
 
+The `#one_movement` method now takes a list of positions and attempts to move
+them together.  It starts each movement attempt with a single position, that of
+the robot.  If any position would move to an obstacle, we abort.  If all new
+positions are open, then we swap _en block_.
+
+Pushing boxes horizontally is the same as before.
+
+Pushing boxes vertically is more complicated.  I thought I could simply grow a
+space above or below the current position and move that recursively.  But there
+is a strange corner case:
+
+```
+..............
+....[][][]....
+....[]..[]....
+.....[][].....
+......[]......
+.......@......
+
+Move: ^
+....[]..[]....
+....[][][]....
+.....[][].....
+......[]......
+.......@......
+..............
+```
+
+The box in the middle, with empty space below it, doesn't get pushed by the
+robot pushing on the other boxes.  Analyzing all the configurations of boxes and
+spaces and their resulting effect would get complicated pretty quickly.  I would
+be better off having a separate method that _attempts_ to move each box before
+it them actually moves them if possible.
+
+Here, I've numbered the boxes so we can see what would happen.
+
+```
+..............
+....668877....
+....44..55....
+.....2233.....
+......11......
+.......@......
+```
+
+```
+Robot wants to go up.
+  Can box 11 go up?
+    Can boxes 22 and 33 go up?
+      Can box 22 go up?
+        Can box 44 go up?
+          Can box 66 go up?
+          Yes
+        Yes
+      Yes
+      Can box 33 go up?
+        Can box 55 go up?
+          Can box 77 go up?
+          Yes
+        Yes
+      Yes
+    Yes
+  Yes
+  Move box 11 up
+    Move boxes 22 and 33 up
+      Move box 22 up
+        Move box 44 up
+          Move box 66 to empty space
+          Move box 44 to empty space
+        Move box 22 to empty space
+      Move box 33 up
+        Move box 55 up
+          Move box 77 to empty space
+          Move box 55 to empty space
+        Move box 33 to empty space
+    Move box 11 to empty space
+  Move robot to empty space
+```
+
+If boxes `66` or `77` are blocked, it prevents anything from moving.  If box
+`88` is blocked, it does not affect anything.
+
+By printing the map on each iteration, the runtime was **3,440ms**.  Without all
+the traces, it came down to **160ms**.
---
+++ day15/puzzle2.rb
@@ -0,0 +1,191 @@
+#!/usr/bin/env ruby
+
+# Login to https://adventofcode.com/2024/day/15/input to download 'input.txt'.
+
+# lines = readlines
+# lines = File.readlines('sample2.txt') # Answer: 9021 (in 76 ms)
+# lines = File.readlines('sample3.txt') # Answer: 618 (in 50 ms)
+lines = File.readlines('input.txt') # Answer: 1524905 (in 160 ms)
+
+Coord = Data.define(:x, :y) do
+  def gps
+    (100 * x) + y
+  end
+
+  def +(other)
+    Coord.new(x + other.x, y + other.y)
+  end
+  
+  def to_s
+    "(#{x}, #{y})"
+  end
+end
+
+Move = Data.define(:x, :y) do
+  def horizontal?
+    x.zero?
+  end
+
+  def to_s
+    "(#{x.positive? ? '+' : ''}#{x}, #{y.positive? ? '+' : ''}#{y})"
+  end
+end
+
+# Renders the map (on *STDOUT* by default)
+def print_map(map, out = $stdout)
+  map.each do |row|
+    out.puts row.join
+  end
+end
+
+separators = lines.map(&:chomp).map.with_index { |line, i| line.empty? ? i : nil }.compact
+
+map = lines[...(separators.first)].map(&:chomp).map do |line|
+  line.split('').map do |char|
+    case char
+    when '#' then ['#', '#']
+    when 'O' then ['[', ']']
+    when '.' then ['.', '.']
+    when '@' then ['@', '.']
+    end
+  end.flatten
+end
+
+puts 'Map'
+puts '---'
+print_map(map)
+puts
+
+robot = map.collect.with_index do |row, x|
+  row.collect.with_index do |cell, y|
+    (cell == '@') ? Coord.new(x, y) : nil
+  end
+end.flatten.compact.first
+
+puts 'Robot'
+puts '-----'
+puts robot
+puts
+
+movements = lines[(separators.first)..].map(&:chomp).join.split('').map do |move|
+  case move
+  when '^' then Move.new(-1, 0)
+  when '>' then Move.new(0, 1)
+  when 'v' then Move.new(1, 0)
+  when '<' then Move.new(0, -1)
+  else throw Exception.new "Illegal movement: #{move}"
+  end
+end
+
+# puts 'Movements'
+# puts '---------'
+# puts movements
+# puts
+
+def swap_single(map, position, new_position)
+  map[position.x][position.y], map[new_position.x][new_position.y] = map[new_position.x][new_position.y], map[position.x][position.y]
+end
+
+def swap_many(map, positions, new_positions)
+  positions.zip(new_positions).each do |position, new_position|
+    swap_single(map, position, new_position)
+  end
+end
+
+def obstructed?(map, positions)
+  positions.any? { |position| map[position.x][position.y] == '#' }
+end
+
+def all_clear?(map, positions)
+  positions.all? { |position| map[position.x][position.y] == '.' }
+end
+
+def can_move?(map, positions, move)
+  new_positions = positions.map { |position| position + move }
+
+  return false if obstructed?(map, new_positions)
+  return true if all_clear?(map, new_positions)
+
+  new_positions.all? do |new_position|
+    case map[new_position.x][new_position.y]
+    when '[' then can_move?(map, [new_position, Coord.new(new_position.x, new_position.y + 1)], move)
+    when ']' then can_move?(map, [Coord.new(new_position.x, new_position.y - 1), new_position], move)
+    when '.' then true
+    else throw Exception.new "Unknown thing at map #{new_position}: #{map[new_position.x][new_position.y]}"
+    end
+  end
+end
+
+def one_movement(map, positions, move)
+  new_positions = positions.map { |position| position + move }
+
+  if obstructed?(map, new_positions)
+    puts 'No nothing'
+    return positions
+  end
+
+  if all_clear?(map, new_positions)
+    puts 'Move to empty space'
+    swap_many(map, positions, new_positions)
+    return new_positions
+  end
+
+  if move.horizontal?
+    puts 'Attempt to move box horizontally'
+    one_movement(map, new_positions, move)
+    if all_clear?(map, new_positions)
+      puts 'Move to now empty space'
+      swap_many(map, positions, new_positions)
+      return new_positions
+    else
+      return positions
+    end
+  else
+    puts 'Attempt to move box(es) vertically'
+
+    box_positions = new_positions.collect do |new_position|
+      case map[new_position.x][new_position.y]
+      when '[' then [new_position, Coord.new(new_position.x, new_position.y + 1)]
+      when ']' then [Coord.new(new_position.x, new_position.y - 1), new_position]
+      when '.' then []
+      else throw Exception.new "Unknown thing at map #{new_position}: #{map[new_position.x][new_position.y]}"
+      end
+    end.flatten.uniq
+
+    if can_move?(map, box_positions, move)
+      one_movement(map, box_positions, move)
+      swap_many(map, positions, new_positions)
+      return new_positions
+    else
+      puts 'Cannot move boxes'
+      return positions
+    end
+  end
+end
+
+movements.each do |move|
+  puts "Move: #{move}"
+  robot = one_movement(map, [robot], move).first
+  # print_map(map)
+  # puts
+end
+
+def get_box_coords(map)
+  map.collect.with_index do |row, x|
+    row.collect.with_index do |cell, y|
+      (cell == '[') ? Coord.new(x, y) : nil
+    end
+  end.flatten.compact
+end
+
+coordinates = get_box_coords(map)
+
+# puts 'Coordinates'
+# puts '-----------'
+# puts coordinates
+# puts
+
+total = coordinates.sum(&:gps)
+
+puts
+puts "Total: #{total}"
