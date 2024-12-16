326f42c577cd1d5b7848a64b8fe3cafa2cfd8a94
Jean Tessier <jean@jeantessier.com>
2024-12-16 05:42:30 -0800

    Fix typo

--- day16/puzzle1_graph.rb
+++ day16/puzzle1_graph.rb
@@ -5,7 +5,7 @@
 # lines = readlines
 # lines = File.readlines('sample1.txt') # Answer: 7036 (in 48 ms)
 # lines = File.readlines('sample2.txt') # Answer: 11048 (in 45 ms)
-lines = File.readlines('input.txt') # Answer: ?90440 (in 117,450 ms)
+lines = File.readlines('input.txt') # Answer: 90440 (in 117,450 ms)
 
 # Renders the map (on *STDOUT* by default)
 def print_map(map, out = $stdout)
