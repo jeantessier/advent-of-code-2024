796060d7e19c5043fe99e6730c5e2857b39e4293
Jean Tessier <jean@jeantessier.com>
2024-12-18 21:22:44 -0800

    Day 19 Puzzle 1

---
+++ day19/README.md
@@ -0,0 +1,10 @@
+# [Day 19](https://adventofcode.com/2024/day/19)
+
+## Puzzle 01
+
+A simple recursive function to check if a pattern or sub-pattern is achievable,
+combined with Ruby's `#any?` method.
+
+## Puzzle 02
+
+
---
+++ day19/puzzle1.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+# Login to https://adventofcode.com/2024/day/19/input to download 'input.txt'.
+
+# lines = readlines
+# lines = File.readlines('sample.txt', chomp: true) # Answer: 6 (in 39 ms)
+lines = File.readlines('input.txt', chomp: true) # Answer: 213 (in 97 ms)
+
+separators = lines.map.with_index { |line, i| line.empty? ? i : nil }.compact
+
+towels = lines[0...(separators.first)].map { |line| line.split(', ') }.flatten
+
+puts 'Towels'
+puts '------'
+puts towels
+puts
+
+wanted_designs = lines[(separators.first + 1)..]
+
+puts 'Wanted Designs'
+puts '--------------'
+puts wanted_designs
+puts
+
+def possible?(design, towels)
+  return true if design.empty?
+
+  towels.any? do |towel|
+    design.start_with?(towel) && possible?(design[(towel.size)..], towels)
+  end
+end
+
+possible_designs = wanted_designs.select do |design|
+  possible?(design, towels)
+end
+
+puts 'Possible Designs'
+puts '----------------'
+puts possible_designs
+puts
+
+answer = possible_designs.size
+
+puts "Answer: #{answer}"
