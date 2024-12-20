9a935ba35556600a6b5343b255710103a42c71b0
Jean Tessier <jean@jeantessier.com>
2024-12-20 10:23:48 -0800

    Clean up stats script

--- stats.rb
+++ stats.rb
@@ -14,7 +14,7 @@
 
 require 'net/http'
 
-STATS_REGEX = %r{<a href="/2024/day/\d+">\s*(?<day>\d+)\s+<span class="stats-both">\s*(?<both>\d+)</span>\s*<span class="stats-firstonly">\s*(?<firstonly>\d+)</span>}
+STATS_REGEX = %r{<a href="/\d+/day/\d+">\s*(?<day>\d+)\s+<span class="stats-both">\s*(?<both>\d+)</span>\s*<span class="stats-firstonly">\s*(?<firstonly>\d+)</span>}
 
 uri = URI.parse('https://adventofcode.com/2024/stats')
 response = Net::HTTP.get_response(uri)
@@ -58,36 +58,36 @@ personal_times = '''
   1   21:55:06  104725      0   22:04:35  98763      0
 '''.lines
    .map(&:chomp)
-   .reject {|line| line.empty?}
+   .reject { |line| line.empty? }
    .map(&:split)
-   .map {|row| [row[0], row[2], row[5]]}
-   .map {|row| row.map(&:to_i)}
+   .map { |row| [row[0], row[2], row[5]] }
+   .map { |row| row.map(&:to_i) }
    .map do |day, my_first_puzzle_rank, my_second_puzzle_rank|
-      total_first_puzzle = overall_stats[day][:finished_first_puzzle]
-      total_second_puzzle = overall_stats[day][:finished_second_puzzle]
-      [
-        day,
-        '',
-        my_first_puzzle_rank.positive? ? my_first_puzzle_rank : '',
-        total_first_puzzle,
-        my_first_puzzle_rank.positive? ? ((1.0 - (my_first_puzzle_rank.to_f / total_first_puzzle)) * 100).to_i : '0',
-        '',
-        my_second_puzzle_rank.positive? ? my_second_puzzle_rank : '',
-        total_second_puzzle,
-        my_second_puzzle_rank.positive? ? ((1.0 - (my_second_puzzle_rank.to_f / total_second_puzzle)) * 100).to_i : '0',
-      ]
+     total_first_puzzle = overall_stats[day][:finished_first_puzzle]
+     total_second_puzzle = overall_stats[day][:finished_second_puzzle]
+     [
+       day,
+       '',
+       my_first_puzzle_rank.positive? ? my_first_puzzle_rank : '',
+       total_first_puzzle,
+       my_first_puzzle_rank.positive? ? ((1.0 - (my_first_puzzle_rank.to_f / total_first_puzzle)) * 100).to_i : '0',
+       '',
+       my_second_puzzle_rank.positive? ? my_second_puzzle_rank : '',
+       total_second_puzzle,
+       my_second_puzzle_rank.positive? ? ((1.0 - (my_second_puzzle_rank.to_f / total_second_puzzle)) * 100).to_i : '0',
+     ]
    end
 
 File.open('stats.csv', 'w', 0644) do |f|
-  f.puts "Day,,Part 1 Rank,Part 1 Total,Part 1 Percentile,,Part 2 Rank,Part 2 Total,Part 2 Percentile"
+  f.puts 'Day,,Part 1 Rank,Part 1 Total,Part 1 Percentile,,Part 2 Rank,Part 2 Total,Part 2 Percentile'
   personal_times.each do |row|
     f.puts row.join(',')
   end
 end
 
 File.open('stats.md', 'w', 0644) do |f|
-  f.puts "| Day |  | Part 1 Rank | Part 1 Total | Part 1 Percentile |  | Part 2 Rank | Part 2 Total | Part 2 Percentile |"
-  f.puts "|:---:|--|:-----------:|:------------:|:-----------------:|--|:-----------:|:------------:|:-----------------:|"
+  f.puts '| Day |  | Part 1 Rank | Part 1 Total | Part 1 Percentile |  | Part 2 Rank | Part 2 Total | Part 2 Percentile |'
+  f.puts '|:---:|--|:-----------:|:------------:|:-----------------:|--|:-----------:|:------------:|:-----------------:|'
   personal_times.each do |row|
     row[0] = format('[%d](day%02d)', row[0], row[0])
     f.puts '| ' + row.join(' | ') + ' |'
