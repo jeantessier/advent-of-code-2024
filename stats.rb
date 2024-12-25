538e39e9e0c86915ad6e4931151aceeae95570a3
Jean Tessier <jean@jeantessier.com>
2024-12-24 21:34:40 -0800

    Update stats

--- stats.rb
+++ stats.rb
@@ -36,6 +36,7 @@ overall_stats = response.body
 
 # https://adventofcode.com/2024/leaderboard/self
 personal_times = '''
+ 24   01:12:16    4506      0   22:34:51   9476      0
  23   01:29:47    5112      0   03:09:55   5501      0
  22   20:46:15   18822      0   22:04:55  15777      0
  21   23:18:24   12227      0       >24h  12104      0
