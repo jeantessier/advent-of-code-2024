92b05b658f5b428d0126d4a284b0babdd075cee5
Jean Tessier <jean@jeantessier.com>
2024-12-24 21:31:32 -0800

    Day 24 Puzzle 2

--- day24/README.md
+++ day24/README.md
@@ -17,3 +17,129 @@ almost 6 quintillions.
 If I walk back from the `z??` wires, I can find all the gates that lead to Z.
 At least one of them must be swapped, otherwise Z wouldn't change.  However,
 when I walk back from Z, I reach each of the 222 gates.
+
+What if I walked back only from the `z??` wires that show broken bits?  The
+higher bits connect to very many gates, but the lower bits have increasingly
+fewer.  Also, `z22` connects to a single gate, so that one will obviously get
+swapped.
+
+Based on this approach (and some inspiration from reading other people's
+approaches), I decided to look at four cases:
+
+1. All `x??` and `y??` wires are set to zero.  I'd expect all `z??` to be zero.
+2. All `x??` wires are set to zero and all `y??` wires are set to one.  I'd expect `z45` to be zero and the other `z??` wires to be ones.
+3. All `x??` wires are set to one and all `y??` wires are set to zero.  I'd expect `z45` to be zero and the other `z??` wires to be ones.
+4. All `x??` and `y??` wires are set to one.  I'd expect `z00` to be zero and the other `z??` wires to be ones.
+
+Without swapping any gates, this is what I get:
+
+```
+0s + 0s
+-------
+expected: 0000000000000000000000000000000000000000000000
+  actual: 0000000000000000000000000000000000000000000000
+ compare:                                               
+
+0s + 1s
+-------
+expected: 0111111111111111111111111111111111111111111111
+  actual: 0111111111111110000000000000001000000011111111
+ compare:                ^^^^^^^^^^^^^^^ ^^^^^^^        
+first bad bit: 8
+
+1s + 0s
+-------
+expected: 0111111111111111111111111111111111111111111111
+  actual: 0111111111111110000000000000001000000011111111
+ compare:                ^^^^^^^^^^^^^^^ ^^^^^^^        
+first bad bit: 8
+
+1s + 1s
+-------
+expected: 1111111111111111111111111111111111111111111110
+  actual: 1111111111111111111111111111110111111011111110
+ compare:                               ^      ^        
+first bad bit: 8
+```
+
+It shows a problem starting at `z08`.  Since this is a adder circuit, it makes
+sense that higher bits depend on the gates of all the lower bits.  So, if I find
+all the gates required up to `z07` and all the gates required up to `z08`, I can
+diff the lists to find the gates related to `z08`.  It is likely that one of
+them is wrong.
+
+`z00` and `z01` had singular lists of gates, but `z02` through `z07` all had
+2x XOR, 2x AND, and 1x OR gates.  `z08` had 3x AND, 1x XOR, and 1x OR gates.
+
+I got the gates for `z07` and drew a diagram of how they are related to each
+other.
+
+```
+wqm XOR dvd -> z07
+vsv OR gpf -> wqm
+x06 AND y06 -> vsv
+gtq AND npm -> gpf
+y07 XOR x07 -> dvd
+
+              x06 A y06       gtq A npm
+                  |               |
+ x07 X y07       vsv -----O----- gpf
+     |                    |
+    dvd --------X--------wqm
+                |
+               z07
+```
+
+And I did the same for `z08`.
+
+```
+dnn AND dqd -> z08
+jmm OR ckq -> dnn
+x07 AND y07 -> jmm
+dvd AND wqm -> ckq
+x08 XOR y08 -> dqd
+
+            x07 A y07       dvd A wqm
+                |               |
+ x08 X y08     jmm -----O----- ckq
+     |                  |
+    dqd -------A-------dnn
+               |
+              z08
+```
+
+That `dnn AND dqd -> z08` at the bottom of the diagram should have been a XOR
+gate.  I found `dqd XOR dnn -> ffj` in the list of gates, so I tried swapping
+`ffj` and `z08`.  My sample computations now worked past bit 8.
+
+```
+0s + 0s
+-------
+expected: 0000000000000000000000000000000000000000000000
+  actual: 0000000000000000000000000000000000000000000000
+ compare:                                               
+
+0s + 1s
+-------
+expected: 0111111111111111111111111111111111111111111111
+  actual: 0111111111111110000000000000000111111111111111
+ compare:                ^^^^^^^^^^^^^^^^               
+first bad bit: 15
+
+1s + 0s
+-------
+expected: 0111111111111111111111111111111111111111111111
+  actual: 0111111111111110000000000000000111111111111111
+ compare:                ^^^^^^^^^^^^^^^^               
+first bad bit: 15
+
+1s + 1s
+-------
+expected: 1111111111111111111111111111111111111111111110
+  actual: 1111111111111111111111111111110111111111111110
+ compare:                               ^               
+first bad bit: 15
+```
+
+I repeated the operation for bits 15, 22, and 31.  Now, having my four pairs,
+I was able to get the answer to the puzzle.
--- day24/puzzle2.rb
+++ day24/puzzle2.rb
@@ -5,8 +5,8 @@ require './gate'
 # Login to https://adventofcode.com/2024/day/24/input to download 'input.txt'.
 
 CONSTANTS = {
-  # file: 'sample3.txt', number_of_pairs: 2, answer: 'z00,z01,z02,z05', time: '?? ms',
-  file: 'input.txt', number_of_pairs: 4, answer: '??', time: '?? ms',
+  # file: 'sample3.txt', answer: 'z00,z01,z02,z05', time: '?? ms',
+  file: 'input.txt', answer: 'dwp,ffj,gjh,jdr,kfm,z08,z22,z31', time: '?? ms',
 }
 
 lines = File.readlines(CONSTANTS[:file], chomp:true)
@@ -15,16 +15,19 @@ separator = lines.map.with_index { |line, i| line.empty? ? i : nil }.compact.fir
 
 WIRE_REGEX = /(?<wire>\w+): (?<value>[01])/
 
+WireBit = Data.define(:wire, :bit, :value)
+
 def bits_from(wires, prefix)
   wires
     .select { |wire, _| wire.start_with?(prefix) }
     .sort
     .reverse
+    .map { |wire, value| WireBit.new wire, wire[1..].to_i, value.to_i }
 end
 
 def number_from(wires, prefix)
-  bits_from(wires, prefix).reduce(0) do |acc, wire|
-    acc | (wire[1] << (wire[0][1..].to_i))
+  bits_from(wires, prefix).reduce(0) do |acc, wire_bit|
+    acc | (wire_bit.value << wire_bit.bit)
   end
 end
 
@@ -35,11 +38,6 @@ wires = lines[...separator]
             hash[match[:wire]] = match[:value].to_i
             hash
           end
-
-puts "Wires (#{wires.size})"
-puts '-----'
-puts wires
-puts
 
 GATE_REGEX = /(?<input1>\w+) (?<type>AND|OR|XOR) (?<input2>\w+) -> (?<output>\w+)/
 
@@ -55,29 +53,14 @@ gates = lines[(separator + 1)..]
   end
 end
 
-puts "Gates (#{gates.size})"
-puts '-----'
-puts gates
-puts
-
-x = number_from(wires, 'x')
-y = number_from(wires, 'y')
-expected_z = x + y
-
-puts 'Numbers'
-puts '-------'
-puts "    x: #{x}"
-puts "    y: #{y}"
-puts "x + y: #{expected_z}"
-puts
-
 def simulate(wires, gates)
   while gates.any?(&:indeterminate?)
     old_count = gates.count(&:indeterminate?)
 
     gates.select(&:indeterminate?).each { |gate| gate.check(wires) }
 
-    break if gates.count(&:indeterminate?) == old_count
+    # break if gates.count(&:indeterminate?) == old_count
+    raise "simulation stalls with #{old_count} gates left" if gates.count(&:indeterminate?) == old_count
   end
 end
 
@@ -85,18 +68,20 @@ def swap_and_simulate(wires, gates, pairs = [])
   cloned_wires = wires.clone
   cloned_gates = gates.collect(&:clone)
 
-  pairs.each do |pair|
-    cloned_gates[pair[0]].output, cloned_gates[pair[1]].output = cloned_gates[pair[1]].output, cloned_gates[pair[0]].output
-  end
+  pairs
+    .map do |pair|
+      pair.map do |output|
+        cloned_gates.find { |gate| gate.output == output }
+      end
+    end
+    .each do |pair|
+      pair[0].output, pair[1].output = pair[1].output, pair[0].output
+    end
 
   simulate(cloned_wires, cloned_gates)
 
-  number_from(cloned_wires, 'z')
+  cloned_wires
 end
-
-initial_z = swap_and_simulate(wires, gates)
-
-outputs = gates.collect { |gate| [gate.output, gate] }.to_h
 
 def dfs(gates, wire)
   gate = gates.find { |gate| gate.output == wire }
@@ -106,43 +91,167 @@ def dfs(gates, wire)
   [gate] + dfs(gates, gate.input1) + dfs(gates, gate.input2)
 end
 
-tree_to_z = gates
-              .map(&:output)
-              .select { |wire| wire.start_with?('z') }
-              .collect { |wire| dfs(gates, wire) }
-              .flatten
-              .uniq
+swaps = [
+  ['z08', 'ffj'],
+  ['dwp', 'kfm'],
+  ['z22', 'gjh'],
+  ['jdr', 'z31'],
+]
 
-puts "Tree to Z (#{tree_to_z.size})"
-puts '---------'
-puts tree_to_z
+puts '0s + 0s'
+puts '-------'
+
+zeroes_plus_zeroes_wires = wires.collect { |k, v| [k, 0] }.to_h
+expected_z_bits = (0..45).to_a.reverse.map { |n| WireBit.new format('z%02d', n), n, 0 }
+
+simulated_wires = swap_and_simulate(zeroes_plus_zeroes_wires, gates, swaps)
+z_bits = bits_from(simulated_wires, 'z')
+
+compare_bits = expected_z_bits.zip(z_bits)
+first_bad_bit = compare_bits.reverse.find { |expected, actual| expected != actual }&.first&.bit
+
+puts 'expected: ' + expected_z_bits.map(&:value).join
+puts '  actual: ' + z_bits.map(&:value).join
+puts ' compare: ' + compare_bits.map { |expected, actual| (expected != actual) ? '^' : ' ' }.join
+
+if first_bad_bit
+  puts "first bad bit: #{first_bad_bit}"
+  good_gates = (0...first_bad_bit).map { |n| format('z%02d', n) }.map { |wire| dfs(gates, wire) }.flatten.uniq
+  bad_gates = dfs(gates, format('z%02d', first_bad_bit)) - good_gates
+
+  puts 'possible bad gates:'
+  puts bad_gates
+end
+
 puts
 
-# candidates = []
-#
-# (0...gates.size).each do |p1a|
-#   ((p1a + 1)...gates.size).each do |p1b|
-#
-#     # puts "Swaping out #{p1a} and #{p1b}"
-#     candidate = [p1a, p1b]
-#     new_z = swap_and_simulate(wires, gates, [candidate])
-#     candidates << candidate if new_z != initial_z
-#   end
-# end
-#
-# puts
-# puts "Candidates (#{candidates.size})"
-# puts '----------'
-# # candidates.each { |c| puts c.inspect }
-# puts
+puts '0s + 1s'
+puts '-------'
 
-# answer = number_from(wires, 'z')
-#
-# puts
-# puts 'Numbers'
-# puts '-------'
-# puts "    x: #{number_from(wires, 'x')}"
-# puts "    y: #{number_from(wires, 'y')}"
-# puts "x + y: #{expected_z_wire}"
-# puts "    z: #{answer}"
-# puts (answer == expected_z_wire) ? '**CORRECT**' : '__wrong__'
+zeroes_plus_ones_wires = wires.collect { |k, v| [k, k.start_with?('x') ? 0 : 1] }.to_h
+expected_z_bits = [WireBit.new('z45', 45, 0)] + (0..44).to_a.reverse.map { |n| WireBit.new format('z%02d', n), n, 1 }
+
+simulated_wires = swap_and_simulate(zeroes_plus_ones_wires, gates, swaps)
+z_bits = bits_from(simulated_wires, 'z')
+
+compare_bits = expected_z_bits.zip(z_bits)
+first_bad_bit = compare_bits.reverse.find { |expected, actual| expected != actual }&.first&.bit
+
+puts 'expected: ' + expected_z_bits.map(&:value).join
+puts '  actual: ' + z_bits.map(&:value).join
+puts ' compare: ' + compare_bits.map { |expected, actual| (expected != actual) ? '^' : ' ' }.join
+
+if first_bad_bit
+  puts "first bad bit: #{first_bad_bit}"
+  good_gates = (0...first_bad_bit).map { |n| format('z%02d', n) }.map { |wire| dfs(gates, wire) }.flatten.uniq
+  bad_gates = dfs(gates, format('z%02d', first_bad_bit)) - good_gates
+
+  puts 'possible bad gates:'
+  puts bad_gates
+end
+
+puts
+
+puts '1s + 0s'
+puts '-------'
+
+ones_plus_zeroes_wires = wires.collect { |k, v| [k, k.start_with?('x') ? 1 : 0] }.to_h
+expected_z_bits = [WireBit.new('z45', 45, 0)] + (0..44).to_a.reverse.map { |n| WireBit.new format('z%02d', n), n, 1 }
+
+simulated_wires = swap_and_simulate(ones_plus_zeroes_wires, gates, swaps)
+z_bits = bits_from(simulated_wires, 'z')
+
+compare_bits = expected_z_bits.zip(z_bits)
+first_bad_bit = compare_bits.reverse.find { |expected, actual| expected != actual }&.first&.bit
+
+puts 'expected: ' + expected_z_bits.map(&:value).join
+puts '  actual: ' + z_bits.map(&:value).join
+puts ' compare: ' + compare_bits.map { |expected, actual| (expected != actual) ? '^' : ' ' }.join
+
+if first_bad_bit
+  puts "first bad bit: #{first_bad_bit}"
+  good_gates = (0...first_bad_bit).map { |n| format('z%02d', n) }.map { |wire| dfs(gates, wire) }.flatten.uniq
+  bad_gates = dfs(gates, format('z%02d', first_bad_bit)) - good_gates
+
+  puts 'possible bad gates:'
+  puts bad_gates
+end
+
+puts
+
+puts '1s + 1s'
+puts '-------'
+
+ones_plus_ones_wires = wires.collect { |k, v| [k, 1] }.to_h
+expected_z_bits = (1..45).to_a.reverse.map { |n| WireBit.new format('z%02d', n), n, 1 } + [WireBit.new('z00', 0, 0)]
+
+simulated_wires = swap_and_simulate(ones_plus_ones_wires, gates, swaps)
+z_bits = bits_from(simulated_wires, 'z')
+
+compare_bits = expected_z_bits.zip(z_bits)
+first_bad_bit = compare_bits.reverse.find { |expected, actual| expected != actual }&.first&.bit
+
+puts 'expected: ' + expected_z_bits.map(&:value).join
+puts '  actual: ' + z_bits.map(&:value).join
+puts ' compare: ' + compare_bits.map { |expected, actual| (expected != actual) ? '^' : ' ' }.join
+
+if first_bad_bit
+  puts "first bad bit: #{first_bad_bit}"
+  good_gates = (0...first_bad_bit).map { |n| format('z%02d', n) }.map { |wire| dfs(gates, wire) }.flatten.uniq
+  bad_gates = dfs(gates, format('z%02d', first_bad_bit)) - good_gates
+
+  puts 'possible bad gates:'
+  puts bad_gates
+end
+
+puts
+
+puts 'Numbers'
+puts '-------'
+
+x = number_from(wires, 'x')
+y = number_from(wires, 'y')
+expected_z = x + y
+
+expected_z_bits = (0..45).to_a.reverse.map { |n| WireBit.new format('z%02d', n), n, (expected_z & (1 << n)).positive? ? 1 : 0 }
+
+simulated_wires = swap_and_simulate(wires, gates, swaps)
+z_bits = bits_from(simulated_wires, 'z')
+
+compare_bits = expected_z_bits.zip(z_bits)
+first_bad_bit = compare_bits.reverse.find { |expected, actual| expected != actual }&.first&.bit
+
+puts 'expected: ' + expected_z_bits.map(&:value).join
+puts '  actual: ' + z_bits.map(&:value).join
+puts ' compare: ' + compare_bits.map { |expected, actual| (expected != actual) ? '^' : ' ' }.join
+
+if first_bad_bit
+  puts "first bad bit: #{first_bad_bit}"
+  good_gates = (0...first_bad_bit).map { |n| format('z%02d', n) }.map { |wire| dfs(gates, wire) }.flatten.uniq
+  bad_gates = dfs(gates, format('z%02d', first_bad_bit)) - good_gates
+
+  puts 'possible bad gates:'
+  puts bad_gates
+end
+
+puts
+
+# first_bad_bit = 31
+
+if first_bad_bit
+  (0..first_bad_bit)
+    .map { |n| format('z%02d', n) }
+    .reduce([]) do |previous_gates, wire|
+    gates_so_far = dfs(gates, wire)
+
+    puts wire
+    puts gates_so_far - previous_gates
+    puts
+
+    gates_so_far
+  end
+end
+
+answer = swaps.flatten.sort.join(',')
+
+puts "Answer: #{answer}"
